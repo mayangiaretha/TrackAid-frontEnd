c75243e41a341ff1cf6f6b85dce10e1a4ec7710d
mayangiaretha <myaretha2620@gmail.com>
2024-04-02 11:40:57 +0300

    added basic textfields

--- src/components/ClientsForm/ClientsForm.jsx
+++ src/components/ClientsForm/ClientsForm.jsx
@@ -1,6 +1,56 @@
-const ClientsForm = ({ username }) => {
-  console.log('got here =========>');
-  return <div>{username}</div>;
+import { Form } from 'formik';
+import { Box, Grid, Stack } from '@mui/material';
+import { TextField } from '../../elements/textfield';
+import { Button } from '../../elements/button';
+import { StyledContainer } from '../../elements/Containers/Containers.styles';
+
+const ClientsForm = ({ formik }) => {
+  const { handleSubmit, values, handleChange, touched, errors } = formik;
+  return (
+    <StyledContainer>
+      <Form id="clientsForm" onSubmit={handleSubmit}>
+        <Grid container spacing={8}>
+          <Grid item xs={12}>
+            <TextField
+              placeholder="Enter client name"
+              name="name"
+              fullWidth
+              variant="standard"
+              value={values.name}
+              onChange={handleChange}
+              error={touched.name && Boolean(errors.name)}
+              helperText={touched.name && errors.name}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              placeholder="Enter client address"
+              name="address"
+              fullWidth
+              variant="standard"
+              value={values.address}
+              onChange={handleChange}
+              error={touched.address && Boolean(errors.address)}
+              helperText={touched.address && errors.address}
+            />
+          </Grid>
+
+          <Grid item xs={6}>
+            <Box mt={7}>
+              <Stack direction="row" gap={2} ml={4}>
+                <Button type="submit" variant="outlined" color="primary">
+                  Save
+                </Button>
+                <Button type="submit" variant="outlined" color="secondary">
+                  Close
+                </Button>
+              </Stack>
+            </Box>
+          </Grid>
+        </Grid>
+      </Form>
+    </StyledContainer>
+  );
 };
 
 export default ClientsForm;
--- src/features/clients/addClients/AddClients.jsx
+++ src/features/clients/addClients/AddClients.jsx
@@ -1,10 +1,30 @@
+import { Formik } from 'formik';
+import { object, string } from 'yup';
+
 import ClientsForm from '../../../components/ClientsForm/ClientsForm';
 import MiniDrawer from '../../../layout/drawer/drawer';
 
 const AddClients = () => {
+  const defaultValues = { name: '', address: '' };
+
+  const yupObject = object({
+    name: string().required('name is required'),
+    address: string().required('address is required'),
+  });
+
+  const handleSubmit = (values) => {
+    const { name, address } = values;
+    console.log(address, 'address', name, 'the name ===============>');
+  };
   return (
     <MiniDrawer>
-      <ClientsForm username="paul" />
+      <Formik
+        initialValues={defaultValues}
+        onSubmit={handleSubmit}
+        validationSchema={yupObject}
+      >
+        {(formik) => <ClientsForm formik={formik} />}
+      </Formik>
     </MiniDrawer>
   );
 };
